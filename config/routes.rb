d5c980013cd76c2a3c41118572ef03b963f1b220
Gorka Echavarri <gorkaechavarri@gmail.com>
2023-06-06 15:02:59 +0100

    minor changes to users validation

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :posts
-  resources :posts, only: %i[index new create edit update destroy]
+  resources :comments, only: %i[index new create edit update destroy]
 end
