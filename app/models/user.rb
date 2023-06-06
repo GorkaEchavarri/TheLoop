649f7711392e2826f3731599876be7733c79f554
Gorka Echavarri <gorkaechavarri@gmail.com>
2023-06-06 16:41:53 +0100

    users can be created with all fields and edited

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,9 @@ class User < ApplicationRecord
   validates :bio, length: { minimum: 8 }
   validates :bio, length: { maximum: 2000 }
 
-  validates :user_type, inclusion: %w[circle patient]
+  validates :user_type, inclusion: %w[circle in-treatment recovered]
+  # Remeber to add in the new user page a stimulus for in-treatment users to add their illness.
+
   # validates :status, inclusion: %w[in-treatment recovered]
 
 end
