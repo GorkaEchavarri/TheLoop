a6515856dfcb0435465c571116fa16354a973e8b
Gorka Echavarri <gorkaechavarri@gmail.com>
2023-06-13 16:28:10 +0100

    working chatrooms with avatars

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -9,7 +9,17 @@ class ChatroomsController < ApplicationController
     # @chatrooms = Chatroom.where(id: @chatroom_ids)
     # @friend_messages = @chatroom.messages.where.not(user_id: current_user.id).pluck(:user_id).first
     @friend = User.find(@chatroom.receiver_id)
+
+    # @best_friend = Chatroom.find(params[:receiver_id])
+    @chatrooms = Chatroom.where(asker_id: current_user.id)
+    @chat_users = []
+    @chatrooms.each { |chatroom| @chat_users << User.find(chatroom.receiver_id) }
+
   end
+
+  # <%# iterate over chatrooms %>
+  # <%# find the user with the id that matches the receiver id on chatroom %>
+  # <%# display the username %>
 
   def new
     @chatroom = Chatroom.new
@@ -30,6 +40,8 @@ class ChatroomsController < ApplicationController
 
   def index
     @chatrooms = Chatroom.all
+    # @best_friend = User.find(@chatroom.receiver_id)
+
   end
 
   private
