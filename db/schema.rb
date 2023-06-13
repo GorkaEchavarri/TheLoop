5379e0606bc6966d828d35a87af7153c27c2cc46
Gorka Echavarri <gorkaechavarri@gmail.com>
2023-06-13 16:46:59 +0100

    migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_12_114150) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_13_084230) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -112,7 +112,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_12_114150) do
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
-    create_table "pg_search_documents", force: :cascade do |t|
+  create_table "pg_search_documents", force: :cascade do |t|
     t.text "content"
     t.string "searchable_type"
     t.bigint "searchable_id"
